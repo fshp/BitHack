20b26746dc4e1e3e4fe2ee5725faff1560b6b36d
Franchesko Salias Hudro Pedros <franchesko.salias.hudro.pedros@gmail.com>
2014-08-13 18:06:26 +0400

    Print public key

--- src/main.c
+++ src/main.c
@@ -6,14 +6,24 @@ int main() {
 
     BIO    *out = NULL;
     EC_KEY *key = NULL;
+    BN_CTX *ctx = NULL;
 
     out = BIO_new_fp(stdout, BIO_FLAGS_WRITE);
+    ctx = BN_CTX_new();
 
     key = EC_KEY_new_by_curve_name(NID_secp256k1);
 
     EC_KEY_generate_key(key);
     EC_KEY_print(out, key, 0);
 
+    const EC_POINT *pub = EC_KEY_get0_public_key(key);
+    char *pub_hex = EC_POINT_point2hex( EC_KEY_get0_group(key), pub,
+                                        EC_KEY_get_conv_form(key), ctx);
+
+    BIO_printf(out, "%s\n", pub_hex);
+
+    OPENSSL_free(pub_hex);
+    BN_CTX_free(ctx);
     EC_KEY_free(key);
     BIO_free(out);
     return (0);
