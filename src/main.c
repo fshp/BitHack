f1baadb2269237e5fdc651317a25fe1ade5aecf9
Franchesko Salias Hudro Pedros <franchesko.salias.hudro.pedros@gmail.com>
2014-08-13 16:29:22 +0400

    Print ECDSA Key

--- src/main.c
+++ src/main.c
@@ -1,3 +1,20 @@
+#include "openssl/ec.h"
+#include "openssl/obj_mac.h"
+#include "openssl/bio.h"
+
 int main() {
-	return 0;
+
+    BIO    *out = NULL;
+    EC_KEY *key = NULL;
+
+    out = BIO_new_fp(stdout, BIO_FLAGS_WRITE);
+
+    key = EC_KEY_new_by_curve_name(NID_secp256k1);
+
+    EC_KEY_generate_key(key);
+    EC_KEY_print(out, key, 0);
+
+    EC_KEY_free(key);
+    BIO_free(out);
+    return (0);
 }
