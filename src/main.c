145e9bb66086c2b682212cc3935fd761c93b6ec3
Franchesko Salias Hudro Pedros <franchesko.salias.hudro.pedros@gmail.com>
2014-08-13 18:59:52 +0400

    Print public key from binary data

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@ int main() {
     BIO    *out = NULL;
     EC_KEY *key = NULL;
     BN_CTX *ctx = NULL;
+    unsigned char buf[1024];
+
 
     out = BIO_new_fp(stdout, BIO_FLAGS_WRITE);
     ctx = BN_CTX_new();
@@ -19,6 +21,31 @@ int main() {
     const EC_POINT *pub = EC_KEY_get0_public_key(key);
     char *pub_hex = EC_POINT_point2hex( EC_KEY_get0_group(key), pub,
                                         EC_KEY_get_conv_form(key), ctx);
+
+    size_t len = EC_POINT_point2oct(EC_KEY_get0_group(key),
+    								pub,
+									EC_KEY_get_conv_form(key),
+									buf,
+									sizeof(buf),
+									ctx);
+
+    if(len != 0 && len < sizeof(buf)) {
+    	buf[len] = 0x00;
+    	//BIO_printf(out, "Bin Pub Address: %s\n", buf);
+    	unsigned i = 0;
+    	for(; i < len; ++i) {
+    		char map[] = "0123456789abcdef";
+    		char tmp[2] = "0";
+    		tmp[0] = map[buf[i] / 16];
+    		BIO_printf(out, tmp);
+    		tmp[0] = map[buf[i] % 16];
+    		BIO_printf(out, tmp);
+    		if(i != len - 1)
+    			BIO_printf(out, ":");
+    	}
+    	BIO_printf(out, "\n");
+
+    }
 
     BIO_printf(out, "%s\n", pub_hex);
 
