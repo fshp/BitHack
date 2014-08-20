ddc4f3890fc473bad067a1a6ad08f6f781baaaa1
Franchesko Salias Hudro Pedros <franchesko.salias.hudro.pedros@gmail.com>
2014-08-21 01:54:10 +0400

    Address in base256

--- src/base58.c
+++ src/base58.c
@@ -3,11 +3,14 @@
 #include <openssl/sha.h>
 #include <openssl/ripemd.h>
 #include <openssl/ec.h>
+#include <string.h>
 
 char* key2addr(const EC_KEY *key) {
 	unsigned char pub_key_oct[65];
 	unsigned char sha_hash[32];
-	unsigned char ripemd_hash[20];
+	unsigned char sha_hash_checksum1[32];
+	unsigned char sha_hash_checksum2[32];
+	unsigned char address_in_base256[1 + 20 + 4] = {0,};
 	SHA256_CTX    sha_ctx;
 	RIPEMD160_CTX ripemd_ctx;
 
@@ -24,12 +27,26 @@ char* key2addr(const EC_KEY *key) {
 
 	RIPEMD160_Init(&ripemd_ctx);
 	RIPEMD160_Update(&ripemd_ctx, &sha_hash, sizeof(sha_hash));
-	RIPEMD160_Final(ripemd_hash, &ripemd_ctx);
+	RIPEMD160_Final(address_in_base256 + 1, &ripemd_ctx);
 
+	SHA256_Init(&sha_ctx);
+	SHA256_Update(&sha_ctx, address_in_base256, sizeof(address_in_base256) - 4);
+	SHA256_Final(sha_hash_checksum1, &sha_ctx);
 
-	BIO_printf(out, "RIPEMD160 of public key: ");
-	for(unsigned i = 0; i < sizeof(ripemd_hash); ++i)
-		BIO_printf(out, "%02x", ripemd_hash[i]);
+	SHA256_Init(&sha_ctx);
+	SHA256_Update(&sha_ctx, sha_hash_checksum1, sizeof(sha_hash_checksum1));
+	SHA256_Final(sha_hash_checksum2, &sha_ctx);
+
+	memcpy(address_in_base256 + 1 + 20, sha_hash_checksum2, 4                  );
+
+	BIO_printf(out, "RIPEMD160 of public key: \t  ");
+	for(unsigned i = 1; i < sizeof(address_in_base256) - 4; ++i)
+		BIO_printf(out, "%02x", address_in_base256[i]);
+	BIO_printf(out, "\n");
+
+	BIO_printf(out, "Addr in Base256: \t\t");
+	for(unsigned i = 0; i < sizeof(address_in_base256); ++i)
+		BIO_printf(out, "%02x", address_in_base256[i]);
 	BIO_printf(out, "\n");
 
 	return 0;
