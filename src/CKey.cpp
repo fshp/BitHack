a767b58a95e7368cdac04b4a28316f439d4b3cfd
Franchesko Salias Hudro Pedros <franchesko.salias.hudro.pedros@gmail.com>
2014-08-23 23:43:17 +0400

    One-line print CKey

--- src/CKey.cpp
+++ src/CKey.cpp
@@ -23,11 +23,9 @@ CKey::~CKey() {
 ostream& operator <<(ostream &os, const CKey &key) {
     boost::format formater("%02x");
 
-    os << "Private key: \t\t";
     for (auto p : key.m_privateKey)
         os << formater % static_cast<uint16_t>(p);
-    os << endl;
-    os << "Address in Ripemd160: \t";
+    os << "\t\t";
     for (auto p : key.m_addressRipemd160)
         os << formater % static_cast<uint16_t>(p);
     os << endl;
